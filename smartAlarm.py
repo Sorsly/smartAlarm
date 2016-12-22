e9a3d49093d4b811d128b88bd452e6d1a7dfefff
Sorsly <han300@purdue.edu>
2016-12-21 20:29:15 -0600

    organized code into classes and methods, added actual time capabilities

--- smartAlarm.py
+++ smartAlarm.py
@@ -1,15 +1,20 @@
 from Tkinter import *
 from datetime import datetime
 
-#init stuff
-top = Tk()
+class TimeApp():
+	def __init__(self):
+		self.root = Tk()	#init stuff
+		self.label = Label(text = "")
+		self.label.pack()
+		self.clock()
+		self.root.mainloop()	#loops main
 
-#GUI stuff
-text = Text(top)
-clock = datetime.now()
-text.insert(INSERT, clock)		#inserts text at location
-text.pack(fill = BOTH)		#geometry organizer
-#loops main?
-top.mainloop()
-top.update_idletasks()
+	def clock(self):
+		clockData = datetime.now()	#fetch current time
+		self.label.configure(text = clockData)
+		#text = Text(self.root)	#text widget that allows text to be placed into window
+		#text.insert(INSERT, clock)		#inserts current time at current location in text
+		#text.pack(fill = BOTH)		#geometry organizer
+		self.root.after(1000, self.clock)	#have self.root call itself after 1000ms
 
+timeApp = TimeApp()
