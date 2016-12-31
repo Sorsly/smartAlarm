952e6d98e2c9dcdabacb0ef151a6f7dcbbbc0eab
Sorsly <han300@purdue.edu>
2016-12-31 15:51:57 -0600

    added some error checking and disabled auto scaling until infinite loop is resolved

--- smartAlarm.py
+++ smartAlarm.py
@@ -19,6 +19,9 @@ class TimeApp():
 		self.bInc = Button(text = "+", command = self.OnInc)
 		self.bDec = Button(text = "-", command = self.OnDec)
 
+		width = self.root.winfo_width()
+		height = self.root.winfo_height()
+
 	#"organize" label wrt window using grid geometry
 		self.labelLeft.grid(row = 0, column = 0)	
 		self.labelClock.grid(row = 1, column = 1)
@@ -29,14 +32,10 @@ class TimeApp():
 		self.root.columnconfigure(1, weight = 1)
 		self.root.rowconfigure(1, weight = 1)
 
-	#Text() way of showing time
-		#self.text = Text(self.root)	#also valid, but specification of window unnecessary here
-		#self.text = Text()		#text widget that allows text to be placed into window
-		#self.text.pack()
-
 		#clock() is a meth that continuously fetches time and updates label
 		self.clock()					#call clock method
-		#self.fontUpdate()
+		#self.fontInit()
+		#self.fontUpdate((2, 2))
 		
 		self.root.mainloop()			#call 'mainloop' from root object to listen for events
 
@@ -44,29 +43,41 @@ class TimeApp():
 		clockData = datetime.now()	#fetch current time
 		#clockData = datetime.strptime("Sun", "%a")	#fetch current time
 
-	#choose Label or Text way of updating time
 	#Label way of updating time
-		#self.labelClock.configure(text = clockData)	#raw time information
+		self.labelClock.configure(text = clockData)	#raw time information
 		self.labelClock.configure(text = datetime.strftime(clockData, "%H:%M:%S\n%a, %b %d"))
-
-	#Text way of updating time
-		#self.text.delete(("0.0"),END)		#inserts current time at current location in text
-		#self.text.insert(INSERT, clockData)		#inserts current time at INSERT index
 
 		self.root.after(1000, self.clock)	#have self.root call itself after 1000ms
 
-	def fontUpdate(self):
+	def fontInit(self):
 		width = self.root.winfo_width()
 		height = self.root.winfo_height()
 		newSizeW = int(round(0.333 * height - 26.667, 0))
-		newSizeH = 0
-		#newSizeH not implemented yet (nonlinear, possibly due to font type)
-		if newSizeW < newSizeH:
-			newSize = newSizeW
-		else:
-			newSize = newSizeH
-		self.clockFont.configure(size = newSize)
-		self.root.after(2000, self.fontUpdate)	#have self.root call itself after 1000ms
+		self.clockFont.configure(size = newSizeW)
+
+		#self.root.after(200, self.fontUpdate((width, height)))	#have self.root call itself after 1000ms
+
+	def fontUpdate(self, (oldWidth, oldHeight)):
+		width = self.root.winfo_width()
+		height = self.root.winfo_height()
+		#if oldWidth != width or oldHeight != height:
+		if 0:
+			newSizeW = int(round(0.333 * height - 26.667, 0))
+			self.clockFont.configure(size = newSizeW)
+
+		width = self.labelClock.winfo_width()
+		widthR = self.labelClock.winfo_reqwidth()
+		height = self.labelClock.winfo_height()
+		heightR = self.labelClock.winfo_reqheight()
+		'''
+		if width < widthR or height < heightR:
+			self.OnInc()
+		elif width > widthR or height > heightR:
+			self.OnDec()
+		'''
+		self.testMeas(self.clockFont['size'])
+		
+		self.root.after(200, self.fontUpdate((width, height)))	#have self.root call itself after 1000ms
 	
 	def OnInc(self):
 		size = self.clockFont['size']
@@ -75,7 +86,10 @@ class TimeApp():
 
 	def OnDec(self):
 		size = self.clockFont['size']
-		self.clockFont.configure(size = size - 2)
+		if size - 2 > 0:
+			self.clockFont.configure(size = size - 2)
+		else:
+			print "smallest possible size!"
 		self.testMeas(size)
 	
 	def testMeas(self, size):	#feel free to remove later; used to figure out window size
